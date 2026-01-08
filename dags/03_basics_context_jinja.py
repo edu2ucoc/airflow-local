dc0440c0d2f6c9fab9e3031fe9e8b8a55ed6c31a
edu2ucoc <edu2.ucoc@gmail.com>
2026-01-08 15:49:06 +0900

    Feat: jinja 및 매크로 적용 완료

--- dags/03_basics_context_jinja.py
+++ dags/03_basics_context_jinja.py
@@ -9,6 +9,10 @@ from airflow.operators.bash import BashOperator
 import logging 
 
 def _print(**kwargs):
+    # airflow context는 함수 호출시 airflow가 injection 하여 전달 -> kwargs
+    # ds, ds_nodash 테스크 수행시간
+    logging.info(f'ds 출력 { kwargs["ds"] }')
+    logging.info(f'ds_nodash 출력 { kwargs["ds_nodash"] }')
     pass
 
 # 실습 DAG 기본 골격(형태) 구성 (02_xxx 참고)
@@ -27,13 +31,18 @@ with DAG(
     tags                = ['jinja','macro', 'context']
 ) as dag:
     # 1. 오퍼레이터 생성
+    #    대부분 오퍼레이터 내부에는 template_field에 jinja가 세팅(허용)되어 있음
+    #    {{ context의키값 }}
     t1 = BashOperator(
         task_id      ='template_used_bash',
-        bash_command =''
+        bash_command ='echo "테스트 수행시간은 {{ ds }}, {{ ds_nodash }} "'
     )
+    # import airflow.macros => 하위 함수들은 private 하게 처리되어 있음 => __ 표기
     t2 = BashOperator(
         task_id      ='template_macro_used_bash',
-        bash_command =''
+        # ETL에서 지난주(어제 혹은 지난달등) 과거 데이터 조회시 필수 패턴중
+        # airflow.macros 패키지를 의미 => import 없이 바로 사용 가능함
+        bash_command ='echo "일주일전 수행시간은 {{ macros.ds_add(ds, -7) }} {{ macros.random() }}" '
     )
     t3 = PythonOperator(
         task_id      ='template_used_python',
