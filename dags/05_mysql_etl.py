928cad60e3dd0fc02c292fb533f93d0a50b8c08b
edu2ucoc <edu2.ucoc@gmail.com>
2026-01-09 13:40:33 +0900

    Feat: Transform 코드 완료

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -41,10 +41,26 @@ def _extract_data_sensor(**kwargs):
     pass
 def _transform_data_std_change(**kwargs):
     # 1. extract_data_sensor task에서 전달한 file_path 획득(XCom 이용)
+    ti             = kwargs['ti']
+    json_file_path = ti.xcom_pull(task_ids='extract_data_sensor')
+    
     # 2. 해당 데이터를 DataFrame으로 로드 (json -> df)
+    df = pd.read_json( json_file_path ) # 경로에 맞춰서 알아서 데이터를 로드
+
     # 3. 전처리 수행 -> 섭씨를 화씨로 변환 처리 ( 화씨(°F) = (섭씨(°C) × 9/5) + 32 )
+    #    컨셉 => 우리 공장에서는 측정온도를 섭씨 100도 이하만 정상 데이터로 간주한다 
+    #           (100도 이상 온도는 이상치로 간주) => 이상치 제거 or 100도 이하만 추출
+    #           => 블리언 인덱싱 : df[ 조건식 ]
+    target_df = df[ df['temperature'] < 100 ].copy() # deep=True) # 데이터 크기 따라 선택
+    #    파생변수로 화씨 데이터로 구성 -> 'temperature_f'
+    target_df['temperature_f'] = (target_df['temperature'] * 9/5) + 32
+
     # 4. 전처리된 데이터를 저장 => 동일 공간에 파일명만 preprocessing_data_{ds_nodash}.csv
+    file_path = f'{DATA_PATH}/preprocessing_data_{ kwargs["ds_nodash"] }.csv'
+    target_df.to_csv( file_path, index=False)
+
     # 5. 저장된 csv 경로를 다음 task에서 사용할 수 있게 반환 처리 (df -> csv, 인덱스제거)
+    return file_path
     pass
 def _load_data_mysql(**kwargs):
     pass
